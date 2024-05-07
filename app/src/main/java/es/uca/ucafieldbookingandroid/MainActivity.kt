610c296dabc102cee626694220bf3f96bdab393b
IsmaelVargasG <is.vargar@alum.uca.es>
2024-05-07 20:25:53 +0200

    Navigation Drawer

--- app/src/main/java/es/uca/ucafieldbookingandroid/MainActivity.kt
+++ app/src/main/java/es/uca/ucafieldbookingandroid/MainActivity.kt
@@ -1,21 +1,74 @@
 package es.uca.ucafieldbookingandroid
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var menu: ImageView
+    private lateinit var salas: TextView
+    private lateinit var reservas: TextView
+    private lateinit var local: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_annadir_reserva)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        menu = findViewById(R.id.menu)
+        salas = findViewById(R.id.boton_salas)
+        reservas = findViewById(R.id.boton_reservas)
+        local = findViewById(R.id.boton_local)
+
+        menu.setOnClickListener {
+            openDrawer(drawerLayout)
         }
+
+        salas.setOnClickListener{
+            redirectActivity(this@MainActivity, Salas::class.java)
+        }
+
+        reservas.setOnClickListener{
+            redirectActivity(this@MainActivity, AnnadirReserva::class.java)
+        }
+
+        local.setOnClickListener{
+            redirectActivity(this@MainActivity, Salas::class.java)
+        }
+
+    }
+    fun openDrawer(drawerLayout: DrawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START)
+    }
+
+    fun closeDrawer(drawerlayout: DrawerLayout){
+        if(drawerlayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+    }
+
+    fun redirectActivity(activity: Activity, secondActivity: Class<*>) {
+        val intent = Intent(activity, secondActivity)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        activity.startActivity(intent)
+        activity.finish()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        closeDrawer(drawerLayout)
     }
 }
--- app/src/main/java/es/uca/ucafieldbookingandroid/Salas.kt
+++ app/src/main/java/es/uca/ucafieldbookingandroid/Salas.kt
@@ -1,19 +1,32 @@
 package es.uca.ucafieldbookingandroid
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.transition.AutoTransition
 import android.transition.TransitionManager
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class Salas : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var menu: ImageView
+    private lateinit var salas: TextView
+    private lateinit var reservas: TextView
+    private lateinit var local: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,13 +40,57 @@ class Salas : AppCompatActivity() {
             Log.d("BUTTONS", "User tapped the Supabutton")
             Toast.makeText(botonExpandir.context, "Boton", Toast.LENGTH_SHORT).show()
             if (expandableView.visibility == View.GONE){
-                TransitionManager.beginDelayedTransition(cardView)
+                TransitionManager.beginDelayedTransition(cardView, AutoTransition())
                 expandableView.visibility = View.VISIBLE
             }
             else{
-                TransitionManager.beginDelayedTransition(cardView)
+                TransitionManager.beginDelayedTransition(cardView, AutoTransition())
                 expandableView.visibility = View.GONE
             }
         }
+
+        drawerLayout = findViewById(R.id.salas)
+        menu = findViewById(R.id.menu)
+        salas = findViewById(R.id.boton_salas)
+        reservas = findViewById(R.id.boton_reservas)
+        local = findViewById(R.id.boton_local)
+
+        menu.setOnClickListener {
+            openDrawer(drawerLayout)
+        }
+
+        salas.setOnClickListener{
+            redirectActivity(this@Salas, Salas::class.java)
+        }
+
+        reservas.setOnClickListener{
+            redirectActivity(this@Salas, AnnadirReserva::class.java)
+        }
+
+        local.setOnClickListener{
+            redirectActivity(this@Salas, Salas::class.java)
+        }
+    }
+
+    fun openDrawer(drawerLayout: DrawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START)
+    }
+
+    fun closeDrawer(drawerlayout: DrawerLayout){
+        if(drawerlayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+    }
+
+    fun redirectActivity(activity: Activity, secondActivity: Class<*>) {
+        val intent = Intent(activity, secondActivity)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        activity.startActivity(intent)
+        activity.finish()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        closeDrawer(drawerLayout)
     }
 }

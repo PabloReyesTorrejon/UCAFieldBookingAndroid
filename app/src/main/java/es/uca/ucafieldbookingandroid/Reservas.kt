c5763055f55a719081147640a6b0a0e604a68171
IsmaelVargasG <is.vargar@alum.uca.es>
2024-05-13 20:34:02 +0200

    Nav Drawer Reservas

--- app/src/main/java/es/uca/ucafieldbookingandroid/Reservas.kt
+++ app/src/main/java/es/uca/ucafieldbookingandroid/Reservas.kt
@@ -1,6 +1,7 @@
 package es.uca.ucafieldbookingandroid
 
 import Reserva
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.transition.TransitionManager
@@ -10,7 +11,9 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
@@ -41,11 +44,19 @@ class Reservas : AppCompatActivity() {
     private lateinit var adapter: ReservaAdapter
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
+
+    private lateinit var salas: TextView
+    private lateinit var vistaReservas: TextView
+    private lateinit var local: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservas)
         val toolbar: Toolbar = findViewById(R.id.my_toolbar)
         setSupportActionBar(toolbar)
+
+        salas = findViewById(R.id.boton_salas)
+        vistaReservas = findViewById(R.id.boton_reservas)
+        local = findViewById(R.id.boton_local)
 
         drawerLayout = findViewById(R.id.drawer_layout)
         toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -79,6 +90,18 @@ class Reservas : AppCompatActivity() {
                 // Muestra un mensaje de error al usuario indicando que el campo está vacío
                 Toast.makeText(this, "Por favor, introduce un ID de usuario", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        salas.setOnClickListener{
+            redirectActivity(this@Reservas, Salas::class.java)
+        }
+
+        vistaReservas.setOnClickListener{
+            redirectActivity(this@Reservas, Reservas::class.java)
+        }
+
+        local.setOnClickListener{
+            redirectActivity(this@Reservas, Salas::class.java)
         }
     }
 
@@ -119,5 +142,27 @@ class Reservas : AppCompatActivity() {
             }
             else -> return super.onOptionsItemSelected(item)
         }
+    }
+
+    fun openDrawer(drawerLayout: DrawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START)
+    }
+
+    fun closeDrawer(drawerlayout: DrawerLayout){
+        if(drawerlayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+    }
+
+    fun redirectActivity(activity: Activity, secondActivity: Class<*>) {
+        val intent = Intent(activity, secondActivity)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        activity.startActivity(intent)
+        /*activity.finish()*/
+    }
+
+    override fun onPause() {
+        super.onPause()
+        closeDrawer(drawerLayout)
     }
 }
